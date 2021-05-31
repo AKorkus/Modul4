1377d26f9ab5b2887da3cd6bf1bfee1d04742e09
AdrianKorkus <adrian.korkus.2017@gmail.com>
2021-05-31 20:09:32 +0200

    Modify Kalkulator.py according to mentor specifications.

--- Kalkulator.py
+++ Kalkulator.py
@@ -1,55 +1,77 @@
 import logging
 from os import name
-import numpy
 
 # Ustawiam log:
 logging.basicConfig(level=logging.DEBUG)
 # , format='%(asctime)s %(message)s'), filename="logfile.log")
 
-
 # Definiuję działania:
-def dodawanie(liczby):
-    return(sum(liczby))
 
 
-def odejmowanie(liczby):
-    l1 = liczby[0]
-    l2 = liczby[1]
-    return l1-l2
+def dodawanie(pierwsza, druga, *args):
+    suma = pierwsza + druga
+    for ingredient in args:
+        suma += ingredient
+    return suma
 
 
-def mnozenie(liczby):
-    return(numpy.product(liczby))
+def odejmowanie(pierwsza, druga):
+    return pierwsza - druga
 
 
-def dzielenie(liczby):
-    l1 = liczby[0]
-    l2 = liczby[1]
-    return l1/l2
+def mnozenie(pierwsza, druga, *args):
+    product = pierwsza * druga
+    for member in args:
+        product *= member
+    return product
 
 
-# Definiuję kalkulator:
-def kalkulator():
+def dzielenie(licznik, mianownik):
+    return licznik/mianownik
 
-    slownik_dzialan = {
-        1: [dodawanie, "dodaję"],
-        2: [odejmowanie, "odejmuję"],
-        3: [mnozenie, "mnożę"],
-        4: [dzielenie, "dzielę"]
-    }
-# Kontrola, czy działanie prawidłowe:
-    while True:
-        try:
-            dzialanie = int(input(
-                "Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:"))
-        except ValueError:
-            logging.warning("Nieprawidłowe działanie!!!")
-        if dzialanie in slownik_dzialan.keys():
-            break
-        else:
-            logging.warning("Nieprawidłowe działanie!!!")
-    # Tutaj wchodzi zaczyt liczb:
-    # Instrukcja:
+
+slownik_dzialan = {
+    1: [dodawanie, "Dodaję"],
+    2: [odejmowanie, "Odejmuję"],
+    3: [mnozenie, "Mnożę"],
+    4: [dzielenie, "Dzielę"]
+}
+
+
+def konwerter(liczba):
+    '''
+    Konwerter przetwarza string "liczba" na float liczba., potrafi czytać ",", ".", "/"):
+    >>> print(konwerter(23,2))
+    23.2
+    >>> print(konwerter(2.3))
+    2.3
+    >>> print(konwerter(3/2))
+    1.5
+    '''
+
+    liczba = liczba.replace(",", ".")
+    # Gdyby ktoś wpisał ułamek zwykły:
+    if "/" in liczba:
+        a, b = liczba.split("/")
+        liczba = float(a)/float(b)
+    liczba = float(liczba)
+    return liczba
+
+# Zaczytanie liczb:
+
+
+def zaczyt_liczb(dzialanie):
+    """
+    Funkcja zaczytuje liczby z inputu użytkownika w zależności od działania:
+    1,3 (dodawanie, mnożenie):
+    Dowolna ilość argumentów.
+    2,4 (odejmowanie, dzielenie):
+    Funkcja zaczytuje maksymalnie 2 argumenty.
+    Zaczytywanie argumentów można zakończyć pustym inputem (sam enter).
+    Wygenerowane inputy przetworzy na listę floatów (przy pomocy konwertera).
+    Przy nieprawidłowym wprowadzeniu liczby raczy poinformować użytkownika o błędzie i zapyta jeszcze raz.
+    """
+
     print("Podaj kolejne liczby, by zakończyć wprowadzanie, wciśnij enter bez podania następnej liczby:")
     print("Możesz podawać liczby całkowite oraz ułamki dziesiętne (, lub .) i ułamki zwykłe z jednym /, powodzenia!")
     i = 1
@@ -57,16 +79,11 @@ def kalkulator():
     while True:
         liczba = input(f"Podaj składnik {i}:")
         # Gdyby ktoś pisał ułamek z przecinkiem:
-        liczba = liczba.replace(",", ".")
         if len(liczba) == 0:
             break
-        # Gdyby ktoś wpisał ułamek zwykły:
         # Tu mogą pojawić się błędy:
         try:
-            if "/" in liczba:
-                liczba = float(liczba.split(
-                    "/")[0]) / float(liczba.split("/")[1])
-            liczba = float(liczba)
+            liczba = konwerter(liczba)
             liczby.append(liczba)
             i += 1
         except:
@@ -74,21 +91,69 @@ def kalkulator():
         # Tutaj ustawiam ogranicznik dzielenia i odejmowania:
         if dzialanie % 2 == 0 and i > 2:
             break
-    # Gdyby ktoś podał jeden składnik w dzieleniu/odejmowaniu:
-    if dzialanie % 2 == 0 and len(liczby) < 2:
-        liczby.append(0+int(dzialanie/4))
-        liczby.append(0+int(dzialanie/4))
-    # Ustawiam raport:
+    # Gdyby ktoś podał jeden składnik lub mniej w dzieleniu/odejmowaniu:
+#    for i in range(len(liczby), 2):
+#        liczby.append(0+int(dzialanie/3))
+    return liczby
+
+
+def wybor_dzialania():
+    """
+    Funkcja służy do wyboru jednego z 4 podstawowych działań:
+    1: dodawanie
+    2: odejmowanie
+    3: mnożenie
+    4: dzielenie
+    Użytkownik musi wpisać jedną z 4 liczb.
+    """
+    while True:
+        dzialanie = 0
+        try:
+            dzialanie = int(input(
+                "Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:"))
+        except ValueError:
+            pass
+        if dzialanie in slownik_dzialan:
+            return dzialanie
+        else:
+            logging.warning("Wpisz jedną z tych liczb: 1, 2, 3, 4!")
+
+
+def raportowanie(dzialanie, liczby):
+    """
+    Funkcja służy informowaniu użytkownika, jakie liczby są składane w działaniu.
+    """
     raport = f"{slownik_dzialan[dzialanie][1]} "
     for i in liczby:
         raport += f"{i} i "
-    raport = raport[:-3]
+    if raport[-3:] == " i ":
+        raport = raport[:-3]
     logging.debug(raport)
-    # Obliczanie wyniku:
+    return raport
 
-    wynik = slownik_dzialan[dzialanie][0](liczby)
-    logging.debug(f"Wynik to: {wynik}")
-    return wynik
+# Definiuję kalkulator:
+
+
+def kalkulator():
+    """
+    Kalkulator składa się z kilku kroków:
+    Wybór działania na podstawie jednego inputu - wywołanie funkcji wybor_dzialania.
+    Wybór argumentów na podstawie serii inputów - wywołanie funkcji zaczyt_liczb.
+    Raportowanie użytkownika o dokonywanym działaniu - wywołanie funkcji raportowanie.
+    Obliczanie wyniku - wywołanie odpowiedniego działania z argumentami, logowanie o wyniku, zwrócenie wyniku.
+    """
+
+    dzialanie = wybor_dzialania()
+    liczby = zaczyt_liczb(dzialanie)
+    raportowanie(dzialanie, liczby)
+
+    # Obliczanie wyniku:
+    try:
+        wynik = slownik_dzialan[dzialanie][0](*liczby)
+        logging.debug(f"Wynik to: {wynik}")
+        return wynik
+    except:
+        logging.debug("Wybrano za mało argumentów!!!")
 
 
 # Wypróbowanie:
